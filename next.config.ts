7ad95d534bc43c361c3619331479e8b94da3af84
nsbradford <nsbradford@gmail.com>
2025-07-13 22:25:44 -0400

    fix nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@google-cloud/bigquery']
+  serverExternalPackages: ['@google-cloud/bigquery'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
