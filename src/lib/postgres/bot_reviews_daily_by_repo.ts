ccc69cf82158fcc36e6f11e5d16e4bfec7803cbb
nsbradford <nsbradford@gmail.com>
2025-09-16 22:47:57 -0700

    BotReviewInRepoDate to any

--- src/lib/postgres/bot_reviews_daily_by_repo.ts
+++ src/lib/postgres/bot_reviews_daily_by_repo.ts
@@ -19,7 +19,7 @@ export function getSql() {
  * @returns Promise<void>
  */
 export async function upsertBotReviewsForDate(
-  botReviews: BotReviewInRepoDate[],
+  botReviews: any[],
   batchSize: number = 1000
 ): Promise<void> {
   if (botReviews.length === 0) {
